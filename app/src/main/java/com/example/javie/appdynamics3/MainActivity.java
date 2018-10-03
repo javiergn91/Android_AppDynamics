105d0cc4c6d90ec2adefc4307d91915938547621
Milton <miltonmja@hotmail.com>
2018-10-02 22:30:36 -0300

    Agregado request http

--- app/src/main/java/com/example/javie/appdynamics3/MainActivity.java
+++ app/src/main/java/com/example/javie/appdynamics3/MainActivity.java
@@ -2,9 +2,23 @@ package com.example.javie.appdynamics3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.appdynamics.eumagent.runtime.Instrumentation;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +27,40 @@ public class MainActivity extends AppCompatActivity {
         Instrumentation.start("AD-AAB-AAM-JTN", getApplicationContext());
 
         setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OkHttpClient client = new OkHttpClient();
+                String url = "https://reqres.in/api/users?page=2";
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if(response.isSuccessful()) {
+                            final String myResponse = response.body().string();
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mTextViewResult.setText(myResponse);
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     }
 }
